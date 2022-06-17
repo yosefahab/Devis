869b2518bbb0eda6510515f35a61924a3d342532
Youssef <YoussefAhab@outlook.com>
2022-06-17 20:07:45 +0200

    Initial Commit

---
+++ Devis/DevisApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevisApp.swift
+//  Devis
+//
+//  Created by Youssef Ahab on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DevisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
